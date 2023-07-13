13e26a2b5f8a556a901e239647ef1a631117d8ce
Alex Galimzyanov <alexofficialkek@gmail.com>
2023-07-13 12:54:53 +0500

    upd dep

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,10 +1,10 @@
 /**
  * @module Helpers
  */
-import * as ora from 'ora';
-
+import ora from 'ora';
+import * as oratype from 'ora';
 /**
  * Create an ora spinner
  * @see https://www.npmjs.com/package/ora
  */
-export const createSpinner = (opts?: ora.Options): ora.Ora => ora(opts);
+export const createSpinner = (opts?: oratype.Options): oratype.Ora => ora(opts);
