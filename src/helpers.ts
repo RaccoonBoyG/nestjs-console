361e64ffa875ec82e0cf6dbe9f880e3844e055b4
Alex Galimzyanov <alexofficialkek@gmail.com>
2023-07-13 13:58:10 +0500

    test

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,10 +1,10 @@
 /**
  * @module Helpers
  */
-import ora from 'ora';
-import * as oratype from 'ora';
+import * as ora from 'ora';
+
 /**
  * Create an ora spinner
  * @see https://www.npmjs.com/package/ora
  */
-export const createSpinner = (opts?: oratype.Options): oratype.Ora => ora(opts);
+export const createSpinner = (opts?: ora.Options): ora.Ora => ora(opts);
--- test/helpers.spec.ts
+++ test/helpers.spec.ts
@@ -3,6 +3,7 @@ import { createSpinner } from '../src/helpers';
 describe('Helpers', () => {
     it('should create an ora spinner', () => {
         const spinner = createSpinner({ text: 'hello' });
+        console.log(spinner);
         expect(spinner).toBeDefined();
     });
 });
